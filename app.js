f967f50e8e8d3fb8b27c7fe110f43ef76c448d2c
igorrybalko <igorrybalko2009@gmail.com>
2021-05-07 20:36:56 +0300

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express'),
+    morgan = require('morgan'),
+    helmet = require('helmet')
+    mongoSanitize = require('express-mongo-sanitize');
+
+const app = express(),
+    port = process.env.PORT || 3001;
+
+app.use(helmet());
+app.use(morgan(process.env.LOG_LEVEL));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(mongoSanitize());
+
+app.listen(port, () => {
+	console.log('server started on http://localhost:' + port);
+})
